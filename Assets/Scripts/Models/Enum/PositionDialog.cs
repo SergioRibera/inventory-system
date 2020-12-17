43a402a41d1d8bd32177421cbb191eebba56d8b6
Sergio Alejandro Ribera Costa <sergioalejandroriberacosta@gmail.com>
2020-12-17 12:55:44 -0400

    Fix push

--- Assets/Scripts/Models/Enum/PositionDialog.cs
+++
@@ -1,6 +0,0 @@
-﻿public enum PositionDialog{
-    Top = 0,
-    Left = 1,
-    Right = 2,
-    Bottom = 3
-}
--- Assets/Scripts/PersistentData.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-public class PersistentData
-{
-    public static string SAVE_PATH = Application.persistentDataPath + "/";
-    public const string KEY_ENCRYPT_DATA = "F-ÑW_SWJDS-ÑBULKER";
-}
